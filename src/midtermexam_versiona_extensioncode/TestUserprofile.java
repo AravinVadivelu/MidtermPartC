0d6d05a41754c62a97055085849447ddb71736d4
Aravin Vadivelu <vadivela@sheridancollege.ca>
2023-02-21 13:58:04 -0500

    Added a TestUserprofile

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author User
+ */
+public class TestUserprofile {
+    public static void main(String[] args) {
+        System.out.println("What is your name?: ");
+        for (int i = 0; i < genres.length; i++){
+            System.out.print(genres[i]);
+        }
+        System.out.println("Choose a genre: ");
+    }
+}
